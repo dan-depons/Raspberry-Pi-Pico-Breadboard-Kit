55c80999da0240cac573c6dfb9bd275dac78a584
deponsdan <daniel.depons@milwaukeetool.com>
2024-08-01 14:51:49 -0500

    updates

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
 from PicoBreadboard import LED,BUZZER,BUTTON
 import time
+import machine
 
 #create object for various class
 LED1 = LED(0) #create object for LED class, LED connected at GP0
 BT1 = BUTTON(4) # Button connected at GP4
 buzzer = BUZZER(8) # Buzzer connected at GP8
+
+# SCK= machine.Pin(18, machine.Pin.ALT_SPI)
+# MOSI= machine.Pin(19, machine.Pin.ALT_SPI)
+# MISO= machine.Pin(16, machine.Pin.ALT_SPI)
+spi0= machine.SPI(0, baudrate=1000000)
+# spi0.init(, pins=(SCK, MISO, MOSI))
 
 while 1:
    if BT1.read() == 1: #button pressed
